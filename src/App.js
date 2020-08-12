735c41b8426a546135a948c98199ece527a04e95
max bischof <max@bischof.dk>
2020-08-12 14:11:49 +0200

    Move state to counter component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Counter from './Counter'
 
 function App() {
-  const [number, setNumber] = useState(0)
-
-  const increaseNumber = () => {
-    setNumber(number + 1)
-  }
-
-  const decreaseNumber = () => {
-    setNumber(number - 1)
-  }
 
   return (
     <div>
-      <Counter
-        increaseNumber={increaseNumber}
-        decreaseNumber={decreaseNumber}
-        number={number}
-      />
+      <Counter />
     </div>
   )
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
 
-function Counter({increaseNumber, decreaseNumber, number}) {
+function Counter() {
+  const [number, setNumber] = useState(0)
+
+  const increaseNumber = () => {
+    setNumber(number + 1)
+  }
+
+  const decreaseNumber = () => {
+    setNumber(number - 1)
+  }
+
   return <div>
   <h1>{number}</h1>
   <Button onclick={increaseNumber} text={'+'} disabled={false} />
